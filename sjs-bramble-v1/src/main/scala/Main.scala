02708289cc84b4eba0d9c214a394f03f3a7cf1c2
Rémi Emonet <remi.emonet@reverse--com.heeere>
2017-11-04 18:47:46 +0100

    Using vue to tune parameters of a scalajs renderer

--- sjs-bramble-v1/src/main/scala/Main.scala
+++ sjs-bramble-v1/src/main/scala/Main.scala
@@ -1,19 +1,29 @@
 import org.scalajs.dom
 import org.scalajs.dom.html
 
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 
 @JSExport
-object BBTest {
+class BSTest (
+  var seed: Int = 20171104,
+  var base: Int = 10,
+  var name: String = "TREE"
+) {
 
   @JSExport
-  def main(canvas: html.Canvas): Unit = {
+  def injectParams(params: js.Dictionary[Any]): Unit = {
+    this.base = params.getOrElse("base", this.base).asInstanceOf[Int]
+  }
+
+  @JSExport
+  def render(canvas: html.Canvas): Unit = {
     val ctx = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-
+    println(seed, base, name)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "green";
-    ctx.fillRect(100, 100, 30, 10)
+    ctx.fillRect(100, 100, 3*base, base)
   }
 }
