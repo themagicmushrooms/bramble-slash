0811d1e8fdc58e467ddb5c55dcb5bde1cdfdf208
Seb E <seb@nospam.org>
2017-11-09 00:23:43 +0100

    Fix off-by-one and make drawing context more stateless

--- sjs-bramble-v1/src/main/scala/Main.scala
+++ sjs-bramble-v1/src/main/scala/Main.scala
@@ -9,8 +9,31 @@ import scala.util.Random
 import scala.collection.mutable.ArrayBuffer
 
 class PosAngle (val x:Double, val y:Double, val a:Double) {
-  def rotateAdvance(da: Double, l: Double) = new PosAngle(x+l*Math.cos(a+da), y+l*Math.sin(a+da), a+da)
-  def squareDistanceTo(cx: Double, cy: Double) = Math.pow((cx-x), 2) + Math.pow((cy-y), 2)
+}
+
+class Point(val x: Double, val y: Double) {
+  def squareDistanceTo(cx: Double, cy: Double) = Math.pow((cx - x), 2) + Math.pow((cy - y), 2)
+  def add(v: Vector) = new Point(x + v.x, y + v.y)
+}
+
+class Vector(val x: Double, val y: Double) {
+  def normalize() = {
+    val norm = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+    new Vector(x / norm, y / norm)
+  }
+  def scale(factor: Double) = new Vector(x * factor, y * factor)
+  def rotate(angle: Double) = new Vector(x * Math.cos(angle) - y * Math.sin(angle), y * Math.cos(angle) + x * Math.sin(angle))
+}
+
+class BranchSegment(val from: Point, val to: Point) {
+  def extend(angle: Double, lengthFactor: Double) = {
+    val straightExtension = new Vector(to.x - from.x, to.y - from.y)
+    val extension = straightExtension.rotate(angle).scale(lengthFactor)
+    new BranchSegment(
+      to,
+      to.add(extension)
+    )
+  }
 }
 
 
@@ -63,36 +86,30 @@ class BSTest (
 
   def drawTree(canvas: html.Canvas, ctx: dom.CanvasRenderingContext2D): Unit = {
     ctx.strokeStyle = "green"
-    ctx.translate(canvas.width / 2, canvas.height)
-    ctx.rotate(-Math.PI / 2)
 
     val r = new Random()
     r.setSeed(seed)
     // TODO breadth first randomized (so adding levels keeps the base shape)
     // ^ not so conveninent from a ux point of view, with the current behavior of fluffyDepth
 
-    def isBranchAllowed(branchOrigin: PosAngle, branchLength: Float): Boolean = {
-      branchOrigin.squareDistanceTo(forbiddenX, forbiddenY) > Math.pow(forbiddenRadius, 2)
+    def isBranchAllowed(firstSegment: BranchSegment): Boolean = {
+      firstSegment.from.squareDistanceTo(forbiddenX, forbiddenY) > Math.pow(forbiddenRadius, 2)
     }
 
-    def drawBranch(depth: Int, firstSegmentWidth: Float, firstSegmentLength: Float, branchOrigin: PosAngle): Unit = {
+    def drawBranch(depth: Int, firstSegmentWidth: Float, firstSegment: BranchSegment): Unit = {
       if (depth < 1) return
-      if (!isBranchAllowed(branchOrigin, firstSegmentLength)) return
+      if (!isBranchAllowed(firstSegment)) return
       ctx.save()
       ctx.beginPath()
-      ctx.moveTo(0, 0)
-      ctx.lineTo(firstSegmentLength, 0)
+      ctx.moveTo(firstSegment.from.x, firstSegment.from.y)
+      ctx.lineTo(firstSegment.to.x, firstSegment.to.y )
       ctx.lineWidth = firstSegmentWidth
       ctx.stroke()
-      ctx.translate(firstSegmentLength, 0)
 
       val childrenWidth = factorWidth * firstSegmentWidth
-      val childrenLength = factorLength * firstSegmentLength
 
       def drawChild(relativeAngle: Double) = {
-        ctx.rotate(relativeAngle)
-        drawBranch(depth - 1, childrenWidth, childrenLength, branchOrigin.rotateAdvance(relativeAngle, childrenLength))
-        ctx.rotate(-relativeAngle)
+        drawBranch(depth - 1, childrenWidth, firstSegment.extend(relativeAngle, factorLength))
       }
 
       val childrenAngles = ArrayBuffer[Double](
@@ -108,7 +125,10 @@ class BSTest (
       ctx.restore()
     }
 
-    drawBranch(depth, baseWidth, baseLength, new PosAngle(canvas.width / 2, canvas.height, -Math.PI / 2))
+    val firstSegment = new BranchSegment(
+      new Point(canvas.width / 2, canvas.height),
+      new Point(canvas.width / 2, canvas.height - baseLength))
+    drawBranch(depth, baseWidth, firstSegment)
   }
 
   def drawForbiddenCircle(ctx: dom.CanvasRenderingContext2D): Any = {
